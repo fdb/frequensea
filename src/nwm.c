b9f0b7820033bbf6485af748084fe9f3ce89444a
Frederik De Bleser <frederik@debleser.be>
2014-12-16 15:29:33 +0100

    Make compatible with 1.8.5.

--- src/nwm.c
+++ src/nwm.c
@@ -23,7 +23,7 @@ GLFWwindow *nwm_create_window(int width, int height) {
     GLFWwindow* window;
     glfwWindowHint(GLFW_DEPTH_BITS, 16);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_SAMPLES, 4);
     #if __APPLE__
