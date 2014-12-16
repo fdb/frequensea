1fe67d96f55c757a82af57beaf120c14af9ec1b5
Frederik De Bleser <frederik@debleser.be>
2014-12-16 15:34:37 +0100

    Update static example to include camera.

--- lua/static.lua
+++ lua/static.lua
@@ -2,12 +2,13 @@
 nwm_init()
 window = nwm_create_window(800, 600)
 
+camera = ngl_new_camera(-20, 18, 50)
 model = ngl_load_obj("../obj/c004.obj")
 shader = ngl_load_shader("../shader/basic.vert", "../shader/basic.frag")
 
 while not nwm_window_should_close(window) do
     ngl_clear(0.2, 0.2, 0.2, 1)
-    ngl_draw_model(model, shader)
+    ngl_draw_model(camera, model, shader)
 
     nwm_poll_events()
     nwm_swap_buffers(window)
