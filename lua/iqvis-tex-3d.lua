029df26675174c533738de9f97260b33fe1e8a76
Frederik De Bleser <frederik@debleser.be>
2015-01-08 11:50:16 +0100

    3D texture lookups.

---
+++ lua/iqvis-tex-3d.lua
@@ -0,0 +1,51 @@
+-- Visualize IQ data as a texture from the HackRF
+-- You want seizures? 'Cause this is how you get seizures.
+
+VERTEX_SHADER = [[
+#version 400
+layout (location = 0) in vec3 vp;
+layout (location = 1) in vec3 vn;
+layout (location = 2) in vec2 vt;
+out vec4 color;
+out vec2 texCoord;
+uniform mat4 uViewMatrix, uProjectionMatrix;
+uniform float uTime;
+uniform sampler2D uTexture;
+void main() {
+    float l = 1.0 - ((vp.x * vp.x + vp.z * vp.z) * 0.04);
+    color = vec4(l, l, l, l);
+    texCoord = vt;
+    float r = texture(uTexture, vt).r * 1;
+
+    vec3 pt = vec3(vp.x, r, vp.z);
+    gl_Position = uProjectionMatrix * uViewMatrix * vec4(pt, 1.0);
+}
+]]
+
+FRAGMENT_SHADER = [[
+#version 400
+in vec4 color;
+in vec2 texCoord;
+layout (location = 0) out vec4 fragColor;
+void main() {
+    fragColor = color;
+}
+]]
+
+function setup()
+    freq = 200.2
+    device = nrf_start(freq, "../rfdata/rf-202.500-2.raw")
+    camera = ngl_camera_init_look_at(0, 2, 4)
+    shader = ngl_shader_init(GL_LINE_STRIP, VERTEX_SHADER, FRAGMENT_SHADER)
+    texture = ngl_texture_create(shader, "uTexture")
+    model = ngl_model_init_grid_triangles(100, 100, 0.1, 0.1)
+end
+
+function draw()
+    ngl_clear(0.2, 0.2, 0.2, 1.0)
+    ngl_texture_update(texture, GL_RED, 512, 512, device.samples)
+    ngl_draw_model(camera, model, shader)
+
+    --nrf_freq_set(device, freq)
+    --freq = freq + 0.0001
+end
