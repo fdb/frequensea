248343f39a26f0b72dd2413c9cfb7c4a9af2cce4
Lieven Menschaert <lievenmenschaert@gmail.com>
2015-01-14 11:09:47 +0100

    Animate sea-model

--- lua/model-sea.lua
+++ lua/model-sea.lua
@@ -15,7 +15,7 @@ float yflow = 4.50;
 vec3 pt = vec3(vp.x + noise1(uTime/10.0-vp.y),vp.y+noise1(vp.x * (roughness* 30.0) + noise1(vp.z * roughness) + uTime * 0.5) *yflow,vp.z);
 
 color = vec3(0.8-noise1(vp.z*innertexture), 1.0-noise1(vp.z*innertexture), 1.0-(noise1(vp.x*innertexture)));
-    gl_Position = uProjectionMatrix * uViewMatrix * vec4(pt, 0.25);
+    gl_Position = uProjectionMatrix * uViewMatrix * vec4(pt, 1.0);
 }
 ]]
 
@@ -29,17 +29,16 @@ void main() {
 ]]
 
 function setup()
-    camera = ngl_camera_init_look_at(-20, 18, 20)
     model = ngl_load_obj("../obj/c004.obj")
     shader = ngl_shader_init(GL_TRIANGLES, VERTEX_SHADER, FRAGMENT_SHADER)
 end
 
 function draw()
-    camera_x = math.sin(nwm_get_time() * 0.3) * 50
-    camera_z = -100
-    camera_y = math.cos(nwm_get_time() * 0.3) * 50
-
-    camera = ngl_camera_init_look_at(camera_x-10, camera_y, camera_z)
     ngl_clear(0.2, 0.2, 0.2, 1)
+    time = nwm_get_time()
+    camera_x = math.sin(time * 0.5) * 50.0
+    camera_z = math.cos(time * 0.5) * 50.0
+    --camera = ngl_camera_init_look_at(camera_x,10,camera_z)
+    camera = ngl_camera_init_look_at(camera_x,30,80)
     ngl_draw_model(camera, model, shader)
 end
