ee77b5dcfc4037c9ceb97b283478ddfb8d63f09a
Frederik De Bleser <frederik@debleser.be>
2015-01-26 22:52:35 +0100

    FM demodulator also accepts file.

--- c/audio.c
+++ c/audio.c
@@ -12,13 +12,16 @@
 #include <libhackrf/hackrf.h>
 #include <sndfile.h>
 
-const int IN_SAMPLE_RATE = 2e6;
+const float TAU = M_PI * 2;
+const int IN_SAMPLE_RATE = 10e6;
 const int OUT_SAMPLE_RATE = 48000;
-
-typedef struct {
-    float x;
-    float y;
-} vec2;
+const int HACKRF_SAMPLES_SIZE = 131072;
+const int HACKRF_BUFFER_SIZE = 262144;
+const int DESIRED_FREQ = 100900000;
+const int FREQ_OFFSET = 50000;
+const int CENTER_FREQ = DESIRED_FREQ + FREQ_OFFSET;
+const int SECONDS = 1;
+const int DESIRED_OUT_SAMPLES = SECONDS * OUT_SAMPLE_RATE;
 
 #define HACKRF_CHECK_STATUS(device, status, message) \
 if (status != 0) { \
@@ -61,25 +64,17 @@ void nal_check_error(const char *file, int line) {
 
 #define NAL_CHECK_ERROR() nal_check_error(__FILE__, __LINE__)
 
-ALuint buffer;
+ALuint audio_buffer = -1;
 ALenum format = AL_FORMAT_MONO16;
-const ALsizei freq = OUT_SAMPLE_RATE;
-const ALsizei size = freq * 3 * sizeof(format);
+
+const ALsizei audio_buffer_size = OUT_SAMPLE_RATE * sizeof(format);
 ALuint source;
 
 ALshort *audio_buffer_samples;
 int has_received = 0;
 
-int received_size = 0;
-vec2 *vec_buffer;
+int received_samples = 0;
 
-const int HACKRF_SAMPLES_SIZE = 131072;
-const int HACKRF_BUFFER_SIZE = 262144;
-
-const float TAU = M_PI * 2;
-
-const int desired_freq = 100.9e6;
-const int center_freq = desired_freq + 100000;
 
 // Generates coefficients for a FIR low-pass filter with the given
 // half-amplitude frequency and kernel length at the given sample rate.
@@ -234,26 +229,39 @@ void shift_frequency(float *samples_i, float *samples_q, int length, int freq_of
     *sine_ptr = sine;
 }
 
-int receive_sample_block(hackrf_transfer *transfer) {
-    if (received_size > size) return 0;
+void process_sample_block(uint8_t *buffer, size_t length) {
+    if (received_samples > DESIRED_OUT_SAMPLES) {
+        return;
+    }
 
     // Convert to floats
     for (int i = 0; i < HACKRF_SAMPLES_SIZE; i++) {
-        unsigned int vi = transfer->buffer[i * 2];
-        unsigned int vq = transfer->buffer[i * 2 + 1];
+        unsigned int vi = buffer[i * 2];
+        unsigned int vq = buffer[i * 2 + 1];
         samples_i[i] = vi / 128.0 - 0.995;
         samples_q[i] = vq / 128.0 - 0.995;
         //samples_i[i] = (vi - 128.0) / 256.0;
         //samples_q[i] = (vq - 128.0) / 256.0;
     }
 
+
+    printf("A %f %f %f %f\n", samples_i[0], samples_q[0], samples_i[1], samples_q[1]);
+
     // Shift frequency
-    int freq_offset = center_freq - desired_freq;
-    shift_frequency(samples_i, samples_q, HACKRF_SAMPLES_SIZE, freq_offset, IN_SAMPLE_RATE, &cosine, &sine);
+    shift_frequency(samples_i, samples_q, HACKRF_SAMPLES_SIZE, FREQ_OFFSET, IN_SAMPLE_RATE, &cosine, &sine);
+
+    printf("B %f %f %f %f\n", samples_i[0], samples_q[0], samples_i[1], samples_q[1]);
 
     // Downsample
     downsampler_process(downsampler_i, samples_i, HACKRF_SAMPLES_SIZE);
     downsampler_process(downsampler_q, samples_q, HACKRF_SAMPLES_SIZE);
+
+    //printf("C %.9f %.9f %.9f %.9f\n", downsampler_i->out_samples[0], downsampler_q->out_samples[0], downsampler_i->out_samples[1], downsampler_q->out_samples[1]);
+
+    for (int i=0;i<10;i++) {
+        printf("C %.9f %.9f\n", downsampler_i->out_samples[i], downsampler_q->out_samples[i]);
+    }
+
 
     // Demodulate
     int out_length = downsampler_i->out_length;
@@ -295,46 +303,51 @@ int receive_sample_block(hackrf_transfer *transfer) {
         prev = demodulated[i];
     }
 
+    for (int i=0;i<10;i++) {
+        printf("D %.9f\n", demodulated[i]);
+    }
+
     // Downsample again
     downsampler_process(downsampler_audio, demodulated, out_length);
-    for (int i = 0; i < out_length; i++) {
-        float f_sample = downsampler_audio->out_samples[i];
-        int16_t i_sample = f_sample * 1000;
-        // if (i == 0) {
-        //     printf("%f %f %d\n", downsampler_audio->out_samples[i], f_sample, i_sample);
-
-        // }
-
-        audio_buffer_samples[received_size++] = i_sample;
+    for (int i=0;i<10;i++) {
+        printf("E %.9f\n", downsampler_audio->out_samples[i]);
     }
 
 
-    //received_size += out_length;
-    printf("Received %.1f%%\n", received_size / (float)size * 100);
+    for (int i = 0; i < out_length; i++) {
+        float f_sample = downsampler_audio->out_samples[i];
+        int16_t i_sample = f_sample * 65000;
+        audio_buffer_samples[received_samples++] = i_sample;
+    }
 
-    if (received_size > size) {
-        FILE *fp = fopen("out.raw", "w");
-        fwrite(audio_buffer_samples, sizeof(int16_t), size, fp);
-        fclose(fp);
+    //received_samples += out_length;
+    printf("Received %.1f%%\n", received_samples / (float) DESIRED_OUT_SAMPLES * 100);
 
+    if (received_samples > DESIRED_OUT_SAMPLES) {
+        //FILE *fp = fopen("out.raw", "w");
+        //fwrite(audio_buffer_samples, sizeof(int16_t), size, fp);
+        //fclose(fp);
 
-        alBufferData(buffer, format, audio_buffer_samples, size, freq);
+        alBufferData(audio_buffer, format, audio_buffer_samples, received_samples * sizeof(ALshort), OUT_SAMPLE_RATE);
         NAL_CHECK_ERROR();
-        alSourcei(source, AL_BUFFER, buffer);
+        alSourcei(source, AL_BUFFER, audio_buffer);
         NAL_CHECK_ERROR();
         alSourcePlay(source);
         NAL_CHECK_ERROR();
 
     }
+}
+
+int receive_sample_block(hackrf_transfer *transfer) {
+    process_sample_block(transfer->buffer, transfer->valid_length);
     return 0;
 }
 
-int main() {
+int main(int argc, char **argv) {
     int status;
-    hackrf_device *hrf;
+    hackrf_device *hrf = NULL;
 
-    audio_buffer_samples = calloc(size + freq, sizeof(ALshort)); // We need a bit of extra data because we go over the buffer size.
-    vec_buffer = calloc(HACKRF_BUFFER_SIZE, sizeof(vec2));
+    audio_buffer_samples = calloc(DESIRED_OUT_SAMPLES + 10000, sizeof(ALshort)); // We need a bit of extra data because we go over the buffer size.
 
     // Initialize the audio context
     ALCdevice *device = alcOpenDevice(NULL);
@@ -347,7 +360,7 @@ int main() {
 
     // Initialize an audio buffer
     alGetError(); // clear error code
-    alGenBuffers(1, &buffer);
+    alGenBuffers(1, &audio_buffer);
     NAL_CHECK_ERROR();
 
     samples_i = calloc(HACKRF_SAMPLES_SIZE, sizeof(float));
@@ -356,14 +369,6 @@ int main() {
     const int INTER_RATE = 336000;
     const int  MAX_F = 75000;
     const float FILTER = MAX_F * 0.8;
-
-
-//   var demodulator = new FMDemodulator(inRate, INTER_RATE, MAX_F, FILTER, 51);
-// function FMDemodulator(inRate, outRate, maxF, filterFreq, kernelLen) {
-//   var coefs = getLowPassFIRCoeffs(inRate, filterFreq, kernelLen);
-//       var downsamplerI = new Downsampler(inRate, outRate, coefs);
-
-
 
     int filter_freq = FILTER * 0.8;
     downsampler_i = downsampler_new(IN_SAMPLE_RATE, INTER_RATE, HACKRF_SAMPLES_SIZE, filter_freq, 51);
@@ -386,42 +391,75 @@ int main() {
     //alSourcePlay(source);
     //NAL_CHECK_ERROR();
 
-    status = hackrf_init();
-    HACKRF_CHECK_STATUS(NULL, status, "hackrf_init");
+    char *fname = NULL;
+    if (argc == 2) {
+        fname = argv[1];
+    }
 
-    status = hackrf_open(&hrf);
-    HACKRF_CHECK_STATUS(hrf, status, "hackrf_open");
+    if (fname == NULL) {
+        // Live data
+        status = hackrf_init();
+        HACKRF_CHECK_STATUS(NULL, status, "hackrf_init");
 
-    status = hackrf_set_freq(hrf, center_freq);
-    HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_freq");
+        status = hackrf_open(&hrf);
+        HACKRF_CHECK_STATUS(hrf, status, "hackrf_open");
 
-    status = hackrf_set_sample_rate(hrf, IN_SAMPLE_RATE);
-    HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_sample_rate");
+        status = hackrf_set_freq(hrf, CENTER_FREQ);
+        HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_freq");
 
-    status = hackrf_set_amp_enable(hrf, 0);
-    HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_amp_enable");
+        status = hackrf_set_sample_rate(hrf, IN_SAMPLE_RATE);
+        HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_sample_rate");
 
-    status = hackrf_set_lna_gain(hrf, 32);
-    HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_lna_gain");
+        status = hackrf_set_amp_enable(hrf, 0);
+        HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_amp_enable");
 
-    status = hackrf_set_vga_gain(hrf, 30);
-    HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_lna_gain");
+        status = hackrf_set_lna_gain(hrf, 32);
+        HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_lna_gain");
 
-    status = hackrf_start_rx(hrf, receive_sample_block, NULL);
-    HACKRF_CHECK_STATUS(hrf, status, "hackrf_start_rx");
+        status = hackrf_set_vga_gain(hrf, 30);
+        HACKRF_CHECK_STATUS(hrf, status, "hackrf_set_lna_gain");
 
+        status = hackrf_start_rx(hrf, receive_sample_block, NULL);
+        HACKRF_CHECK_STATUS(hrf, status, "hackrf_start_rx");
+
+        // Wait 3 seconds to receive data.
+        sleep(1);
+
+    } else {
+        FILE *fp = fopen(fname, "r");
+        fseek(fp, 0L, SEEK_END);
+        long size = ftell(fp);
+        rewind(fp);
+        uint8_t *file_buffer = calloc(size, 1);
+        fread(file_buffer, size, 1, fp);
+        fclose(fp);
+
+        int i = 0;
+        while (1) {
+            process_sample_block(file_buffer + i, HACKRF_BUFFER_SIZE);
+            i += HACKRF_BUFFER_SIZE;
+            if (i >= size) {
+                break;
+            }
+        }
+
+    }
 
     // Playing is asynchronous so wait a while
-    sleep(3 * 2);
+    sleep(1);
 
     // Cleanup
-    alDeleteBuffers(1, &buffer);
+    if (audio_buffer != -1) {
+        alDeleteBuffers(1, &audio_buffer);
+    }
     alcMakeContextCurrent(NULL);
     alcDestroyContext(ctx);
     alcCloseDevice(device);
 
-    hackrf_stop_rx(hrf);
-    hackrf_close(hrf);
-    hackrf_exit();
+    if (fname == NULL) {
+        hackrf_stop_rx(hrf);
+        hackrf_close(hrf);
+        hackrf_exit();
+    }
     return 0;
 }
