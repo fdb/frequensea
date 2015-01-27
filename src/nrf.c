b2542d215f76d1a71ab794736a568989b3121ad5
Frederik De Bleser <frederik@debleser.be>
2015-01-27 14:41:18 +0100

    Add basic DSP building blocks to NRF.

--- src/nrf.c
+++ src/nrf.c
@@ -13,6 +13,8 @@
 
 #include "nrf.h"
 
+const double TAU = M_PI * 2;
+
 void _nrf_rtlsdr_check_status(nrf_device *device, int status, const char *message, const char *file, int line) {
     if (status != 0) {
         fprintf(stderr, "RTL-SDR: %s (Status code %d) %s:%d\n", message, status, file, line);
@@ -324,4 +326,303 @@ void nrf_device_free(nrf_device *device) {
     fftw_free(device->fft_in);
     fftw_free(device->fft_out);
     free(device);
+}
+
+// Finite Impulse Response (FIR) Filter
+
+// Generates coefficients for a FIR low-pass filter with the given
+// half-amplitude frequency and kernel length at the given sample rate.
+//
+// sample_rate    - The signal's sample rate.
+// half_ampl_freq - The half-amplitude frequency in Hz.
+// length         - The filter kernel's length. Should be an odd number.
+//
+// Returns the FIR coefficients for the filter.
+double *nrf_fir_get_low_pass_coefficients(int sample_rate, int half_ampl_freq, int length) {
+    length += (length + 1) % 2;
+    double freq = half_ampl_freq / (double) sample_rate;
+    double *coefs = calloc(length, sizeof(double));
+    int center = floor(length / 2);
+    double sum = 0;
+    for (int i = 0; i < length; i++) {
+        double val;
+        if (i == center) {
+            val = TAU * freq;
+        } else {
+            double angle = TAU * (i + 1) / (double) (length + 1);
+            val = sin(TAU * freq * (i - center)) / (double) (i - center);
+            val *= 0.42 - 0.5 * cos(angle) + 0.08 * cos(2 * angle);
+        }
+        sum += val;
+        coefs[i] = val;
+    }
+    for (int i = 0; i < length; i++) {
+        coefs[i] /= sum;
+    }
+    return coefs;
+}
+
+nrf_fir_filter *nrf_fir_filter_new(int sample_rate, int half_ampl_freq, int length) {
+    nrf_fir_filter *filter = calloc(1, sizeof(nrf_fir_filter));
+    filter->length = length;
+    filter->coefficients = nrf_fir_get_low_pass_coefficients(sample_rate, half_ampl_freq, length);
+    filter->offset = length - 1;
+    filter->center = floor(length / 2);
+    filter->samples_length = filter->offset;
+    filter->samples = calloc(filter->samples_length, sizeof(double));
+    return filter;
+}
+
+void nrf_fir_filter_load(nrf_fir_filter *filter, double *samples, int length) {
+    int should_free = 0;
+    int new_length = length + filter->offset;
+    double *new_samples;
+    if (filter->samples_length != new_length) {
+        should_free = 1;
+        new_samples = calloc(new_length, sizeof(double));
+    } else {
+        new_samples = filter->samples;
+    }
+
+    // Copy the last `offset` samples to the new buffer.
+    memcpy(new_samples, filter->samples + filter->samples_length - filter->offset, filter->offset * sizeof(double));
+    memcpy(new_samples + filter->offset, samples, length * sizeof(double));
+
+    if (should_free) {
+        free(filter->samples);
+    }
+    filter->samples = new_samples;
+    filter->samples_length = new_length;
+}
+
+double nrf_fir_filter_get(nrf_fir_filter *filter, int index) {
+    double v = 0;
+    for (int i = 0; i < filter->length; i++) {
+        v += filter->coefficients[i] * filter->samples[index + i];
+    }
+    return v;
+}
+
+void nrf_fir_filter_free(nrf_fir_filter *filter) {
+    free(filter->coefficients);
+    free(filter->samples);
+    free(filter);
+}
+
+// Downsampler
+
+nrf_downsampler *nrf_downsampler_new(int in_rate, int out_rate, int filter_freq, int kernel_length) {
+    nrf_downsampler *d = calloc(1, sizeof(nrf_downsampler));
+    d->in_rate = in_rate;
+    d->out_rate = out_rate;
+    d->filter = nrf_fir_filter_new(in_rate, filter_freq, kernel_length);
+    d->rate_mul = in_rate / (double) out_rate;
+    d->out_length = 0;
+    d->out_samples = NULL;
+    return d;
+}
+
+void nrf_downsampler_process(nrf_downsampler *d, double *samples, int length) {
+    nrf_fir_filter_load(d->filter, samples, length);
+
+    // FIXME: Optimize by comparing out_length to d->out_length
+    free(d->out_samples);
+    d->out_length = floor(length / d->rate_mul);
+    d->out_samples = calloc(d->out_length, sizeof(double));
+
+    double t = 0;
+    for (int i = 0; i < d->out_length; i++) {
+        d->out_samples[i] = nrf_fir_filter_get(d->filter, floor(t));
+        t += d->rate_mul;
+    }
+}
+
+void nrf_downsampler_free(nrf_downsampler *d) {
+    nrf_fir_filter_free(d->filter);
+    free(d->out_samples);
+    free(d);
+}
+
+// Frequency shifter
+
+nrf_freq_shifter *nrf_freq_shifter_new(int freq_offset, int sample_rate) {
+    nrf_freq_shifter *shifter = calloc(1, sizeof(nrf_freq_shifter));
+    shifter->freq_offset = freq_offset;
+    shifter->sample_rate = sample_rate;
+    shifter->cosine = 1;
+    shifter->sine = 1;
+    return shifter;
+}
+
+void nrf_freq_shifter_process(nrf_freq_shifter *shifter, double *samples_i, double *samples_q, int length) {
+    double delta_cos = cos(TAU * shifter->freq_offset / (double) shifter->sample_rate);
+    double delta_sin = sin(TAU * shifter->freq_offset / (double) shifter->sample_rate);
+    int cosine = shifter->cosine;
+    int sine = shifter->sine;
+    for (int i = 0; i < length; i++) {
+        double vi = samples_i[i];
+        double vq = samples_q[i];
+        samples_i[i] = vi * cosine - vq * sine;
+        samples_q[i] = vi * sine + vq * cosine;
+        double new_sine = cosine * delta_sin + sine * delta_cos;
+        double new_cosine = cosine * delta_cos - sine * delta_sin;
+        sine = new_sine;
+        cosine = new_cosine;
+    }
+    shifter->cosine = cosine;
+    shifter->sine = sine;
+}
+
+void nrf_freq_shifter_free(nrf_freq_shifter *shifter) {
+    free(shifter);
+}
+
+// FM Demodulator
+
+nrf_fm_demodulator *nrf_fm_demodulator_new(int in_sample_rate, int out_sample_rate) {
+    nrf_fm_demodulator *d = calloc(1, sizeof(nrf_fm_demodulator));
+    const int inter_rate = 336000;
+    const int  max_f = 75000;
+    const double filter = max_f * 0.8;
+    d->in_sample_rate = in_sample_rate;
+    d->out_sample_rate = out_sample_rate;
+    d->ampl_conv = out_sample_rate / (TAU * max_f);
+    int filter_freq = filter * 0.8;
+    d->downsampler_i = nrf_downsampler_new(in_sample_rate, inter_rate, filter_freq, 51);
+    d->downsampler_q = nrf_downsampler_new(in_sample_rate, inter_rate, filter_freq, 51);
+    d->downsampler_audio = nrf_downsampler_new(inter_rate, out_sample_rate, 10000, 41);
+    return d;
+}
+
+void nrf_fm_demodulator_process(nrf_fm_demodulator *demodulator, double *samples_i, double *samples_q, int length) {
+    // Downsample
+    nrf_downsampler_process(demodulator->downsampler_i, samples_i, length);
+    nrf_downsampler_process(demodulator->downsampler_q, samples_q, length);
+
+    // Allocate demodulated samples buffer
+    int demodulated_length = demodulator->downsampler_i->out_length;
+    if (demodulated_length != demodulator->demodulated_length) {
+        free(demodulator->demodulated_samples);
+        demodulator->demodulated_samples = calloc(demodulated_length, sizeof(double));
+        demodulator->demodulated_length = demodulated_length;
+    }
+    double *demodulated_samples = demodulator->demodulated_samples;
+
+    // Actual FM demodulation
+    double prev = 0;
+    double delta_sum_squared = 0;
+    double l_i = demodulator->l_i;
+    double l_q = demodulator->l_q;
+    for (int i = 0; i < demodulated_length; i++) {
+        double real = l_i * demodulator->downsampler_i->out_samples[i] + l_q * demodulator->downsampler_q->out_samples[i];
+        double imag = l_i * demodulator->downsampler_q->out_samples[i] - demodulator->downsampler_i->out_samples[i] * l_q;
+        double sgn = 1;
+        if (imag < 0) {
+            sgn *= -1;
+            imag *= -1;
+        }
+        double ang = 0;
+        double div;
+        if (real == imag) {
+            div = 1;
+        } else if (real > imag) {
+            div = imag / real;
+        } else {
+            ang = -M_PI / 2;
+            div = real / imag;
+            sgn *= -1;
+        }
+        demodulated_samples[i] = sgn *
+            (ang + div
+                / (0.98419158358617365
+                    + div * (0.093485702629671305
+                        + div * 0.19556307900617517))) * demodulator->ampl_conv;
+        l_i = demodulator->downsampler_i->out_samples[i];
+        l_q = demodulator->downsampler_q->out_samples[i];
+        double delta = prev - demodulated_samples[i];
+        delta_sum_squared += delta * delta;
+        prev = demodulated_samples[i];
+    }
+    demodulator->l_i = l_i;
+    demodulator->l_q = l_q;
+
+    // Downsample again, for audio
+    nrf_downsampler_process(demodulator->downsampler_audio, demodulated_samples, demodulated_length);
+
+   // Copy audio samples to demodulator
+    int audio_length = demodulator->downsampler_audio->out_length;
+    if (audio_length != demodulator->audio_length) {
+        free(demodulator->audio_samples);
+        demodulator->audio_samples = calloc(audio_length, sizeof(double));
+        demodulator->audio_length = audio_length;
+    }
+    memcpy(demodulator->audio_samples, demodulator->downsampler_audio->out_samples, audio_length * sizeof(double));
+    double *audio_samples = demodulator->audio_samples;
+
+    // De-emphasize samples
+    double alpha = 1.0 / (1.0 + demodulator->out_sample_rate * 50.0 / 1e6);
+    double val = demodulator->deemphasis_val;
+    for (int i = 0; i < audio_length; i++) {
+        val = val + alpha * (audio_samples[i] - val);
+        audio_samples[i] = val;
+    }
+    demodulator->deemphasis_val = val;
+}
+
+void nrf_fm_demodulator_free(nrf_fm_demodulator *demodulator) {
+    nrf_downsampler_free(demodulator->downsampler_i);
+    nrf_downsampler_free(demodulator->downsampler_q);
+    nrf_downsampler_free(demodulator->downsampler_audio);
+    free(demodulator->demodulated_samples);
+    free(demodulator->audio_samples);
+    free(demodulator);
+}
+
+// Decoder
+
+nrf_decoder *nrf_decoder_new(nrf_demodulate_type demodulate_type, int in_sample_rate, int out_sample_rate, int freq_offset) {
+    nrf_decoder *decoder = calloc(1, sizeof(nrf_decoder));
+    decoder->demodulate_type = demodulate_type;
+    if (decoder->demodulate_type == NRF_DEMODULATE_WBFM) {
+        decoder->demodulator = nrf_fm_demodulator_new(decoder->in_sample_rate, decoder->out_sample_rate);
+    }
+    decoder->in_sample_rate = in_sample_rate;
+    decoder->out_sample_rate = out_sample_rate;
+    decoder->freq_shifter = nrf_freq_shifter_new(freq_offset, in_sample_rate);
+    return decoder;
+}
+
+void nrf_decoder_process(nrf_decoder *decoder, uint8_t *buffer, size_t length) {
+    // Convert 8-bit samples to doubles
+    if (decoder->samples_length != length) {
+        free(decoder->samples_i);
+        free(decoder->samples_q);
+        decoder->samples_i = calloc(length, sizeof(double));
+        decoder->samples_q = calloc(length, sizeof(double));
+    }
+
+    double *samples_i = decoder->samples_i;
+    double *samples_q = decoder->samples_q;
+    for (int i = 0; i < length; i++) {
+        unsigned int vi = buffer[i * 2];
+        unsigned int vq = buffer[i * 2 + 1];
+        samples_i[i] = vi / 128.0 - 0.995;
+        samples_q[i] = vq / 128.0 - 0.995;
+        //samples_i[i] = (vi - 128.0) / 128.0;
+        //samples_q[i] = (vq - 128.0) / 128.0;
+    }
+
+    // Shift frequency
+    nrf_freq_shifter_process(decoder->freq_shifter, samples_i, samples_q, length);
+
+    // Demodulate
+    if (decoder->demodulate_type == NRF_DEMODULATE_WBFM) {
+        nrf_fm_demodulator *demodulator = (nrf_fm_demodulator*) decoder->demodulator;
+        nrf_fm_demodulator_process(demodulator, samples_i, samples_q, length);
+    }
+}
+
+void nrf_decoder_free(nrf_decoder *decoder) {
+    nrf_freq_shifter_free(decoder->freq_shifter);
+    free(decoder);
 }
--- src/nrf.h
+++ src/nrf.h
@@ -5,6 +5,8 @@
 
 #include <fftw3.h>
 #include <pthread.h>
+#include <OpenAL/al.h>
+#include <OpenAL/alc.h>
 
 #include "vec.h"
 
@@ -12,6 +14,8 @@
 #define NRF_SAMPLES_SIZE 131072
 #define FFT_SIZE 1024
 #define FFT_HISTORY_SIZE 512
+
+// Device
 
 typedef enum {
     NRF_DEVICE_DUMMY = 0,
@@ -47,5 +51,101 @@ typedef struct {
 nrf_device *nrf_device_new(double freq_mhz, const char* data_file, float interpolate_step);
 double nrf_device_set_frequency(nrf_device *device, double freq_mhz);
 void nrf_device_free(nrf_device *device);
+
+// Finite Impulse Response (FIR) Filter
+
+typedef struct {
+    int length;
+    double *coefficients;
+    int offset;
+    int center;
+    int samples_length;
+    double *samples;
+} nrf_fir_filter;
+
+double *nrf_fir_get_low_pass_coefficients(int sample_rate, int half_ampl_freq, int length);
+nrf_fir_filter *nrf_fir_filter_new(int sample_rate, int half_ampl_freq, int length);
+void nrf_fir_filter_load(nrf_fir_filter *filter, double *samples, int length);
+double nrf_fir_filter_get(nrf_fir_filter *filter, int index);
+void nrf_fir_filter_free(nrf_fir_filter *filter);
+
+// Downsampler
+
+typedef struct {
+    int in_rate;
+    int out_rate;
+    nrf_fir_filter *filter;
+    double rate_mul;
+    int out_length;
+    double *out_samples;
+} nrf_downsampler;
+
+nrf_downsampler *nrf_downsampler_new(int in_rate, int out_rate, int filter_freq, int kernel_length);
+void nrf_downsampler_process(nrf_downsampler *d, double *samples, int length);
+void nrf_downsampler_free(nrf_downsampler *d);
+
+// Frequency shifter
+
+typedef struct {
+    int freq_offset;
+    int sample_rate;
+    double cosine;
+    double sine;
+} nrf_freq_shifter;
+
+nrf_freq_shifter *nrf_freq_shifter_new(int freq_offset, int sample_rate);
+void nrf_freq_shifter_process(nrf_freq_shifter *shifter, double *samples_i, double *samples_q, int length);
+void nrf_freq_shifter_free(nrf_freq_shifter *shifter);
+
+// Demodulator
+
+typedef struct {
+    int size;
+    int capacity;
+    ALuint *values;
+} _nrf_buffer_queue;
+
+typedef struct {
+    int in_sample_rate;
+    int out_sample_rate;
+    double ampl_conv;
+    double l_i;
+    double l_q;
+    double deemphasis_val;
+    nrf_downsampler *downsampler_i;
+    nrf_downsampler *downsampler_q;
+    nrf_downsampler *downsampler_audio;
+    double *demodulated_samples;
+    int demodulated_length;
+    double *audio_samples;
+    int audio_length;
+} nrf_fm_demodulator;
+
+nrf_fm_demodulator *nrf_fm_demodulator_new(int in_sample_rate, int out_sample_rate);
+void nrf_fm_demodulator_process(nrf_fm_demodulator *demodulator, double *samples_i, double *samples_q, int length);
+void nrf_fm_demodulator_free(nrf_fm_demodulator *demodulator);
+
+// Decoder
+
+typedef enum {
+    NRF_DEMODULATE_RAW = 0,
+    NRF_DEMODULATE_WBFM
+} nrf_demodulate_type;
+
+typedef struct {
+    int in_sample_rate;
+    int out_sample_rate;
+    nrf_demodulate_type demodulate_type;
+    void *demodulator;
+    nrf_freq_shifter *freq_shifter;
+    double *samples_i;
+    double *samples_q;
+    int samples_length;
+} nrf_decoder;
+
+
+nrf_decoder *nrf_decoder_new(nrf_demodulate_type demodulate_type, int in_sample_rate, int out_sample_rate, int freq_offset);
+void nrf_decoder_process(nrf_decoder *decoder, uint8_t *buffer, size_t length);
+
 
 #endif // NRF_H
