647a61b165e7866520a2acb4d94f6ac6f77af26a
Frederik De Bleser <frederik@debleser.be>
2015-01-29 20:46:11 +0100

    Subtle changes to iq-tex.lua for better animation.

--- lua/iq-tex.lua
+++ lua/iq-tex.lua
@@ -12,7 +12,7 @@ uniform mat4 uViewMatrix, uProjectionMatrix;
 uniform float uTime;
 void main() {
     color = vec3(1.0, 1.0, 1.0);
-    texCoord = vt; // vec2(vp.x + 0.5, vp.z + 0.5);
+    texCoord = vt;
     gl_Position = vec4(vp.x*2, vp.z*2, 0, 1.0);
 }
 ]]
@@ -24,14 +24,14 @@ in vec2 texCoord;
 uniform sampler2D uTexture;
 layout (location = 0) out vec4 fragColor;
 void main() {
-    float r = texture(uTexture, texCoord).r * 0.02;
+    float r = texture(uTexture, texCoord).r * 0.01;
     fragColor = vec4(r, r, r, 0.95);
 }
 ]]
 
 function setup()
-    freq = 200.5
-    device = nrf_device_new(freq, "../rfdata/rf-200.500-big.raw")
+    freq = 200
+    device = nrf_device_new(freq, "../rfdata/rf-200.500-big.raw", 0.999)
     camera = ngl_camera_new_look_at(0, 0, 0) -- Camera is unnecessary but ngl_draw_model requires it
     shader = ngl_shader_new(GL_TRIANGLES, VERTEX_SHADER, FRAGMENT_SHADER)
     texture = ngl_texture_new(shader, "uTexture")
