b00578620ce3901ce10eaf3fcc0233640e556f6e
Frederik De Bleser <frederik@debleser.be>
2015-01-29 22:50:44 +0100

    Slinky back to black.

--- lua/slinky.lua
+++ lua/slinky.lua
@@ -8,7 +8,7 @@ out vec3 color;
 uniform mat4 uViewMatrix, uProjectionMatrix;
 uniform float uTime;
 void main() {
-    color = vec3(0.1, 0.1, 0.1);
+    color = vec3(0.95, 0.95, 0.95);
     vec3 pt = vec3((vp.x - 0.5) * 5, (vp.y - 0.5) * 5, (vp.z - 0.5) * 100);
     gl_Position = uProjectionMatrix * uViewMatrix * vec4(pt, 1.0);
     gl_PointSize = 3;
@@ -26,15 +26,15 @@ void main() {
 
 camera_x = 0
 camera_y = 0
-camera_z = 53
+camera_z = 51
 function setup()
-    freq = 1.9445
+    freq = 203.5
     device = nrf_device_new(freq, "../rfdata/rf-100.900-2.raw")
     shader = ngl_shader_new(GL_LINE_STRIP, VERTEX_SHADER, FRAGMENT_SHADER)
 end
 
 function draw()
-    ngl_clear(0.95, 0.95, 0.95, 1.0)
+    ngl_clear(0.05, 0.05, 0.05, 1.0)
     camera = ngl_camera_new_look_at(camera_x, camera_y, camera_z)
     model = ngl_model_new(3, NRF_SAMPLES_SIZE, device.samples)
     ngl_draw_model(camera, model, shader)
