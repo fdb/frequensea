7c80bc2615552591b6c1262583fecd0e60a04bd6
Frederik De Bleser <frederik@debleser.be>
2015-02-03 11:28:08 +0100

    Use Bresenham line drawing, make result bigger.

--- c/single-sample.c
+++ c/single-sample.c
@@ -10,9 +10,11 @@
 #include "easypng.h"
 
 const int SAMPLES_STEP = 100;
-const int WIDTH = 256;
-const int HEIGHT = 256;
-const int PIXEL_INC = 3;
+const int IQ_RESOLUTION = 256;
+const int SIZE_MULTIPLIER = 4;
+const int WIDTH = IQ_RESOLUTION * SIZE_MULTIPLIER;
+const int HEIGHT = IQ_RESOLUTION * SIZE_MULTIPLIER;
+const int PIXEL_INC = 4;
 
 void pixel_put(uint8_t *image_buffer, int x, int y, int color) {
     int offset = y * WIDTH + x;
@@ -20,10 +22,14 @@ void pixel_put(uint8_t *image_buffer, int x, int y, int color) {
 }
 
 void pixel_inc(uint8_t *image_buffer, int x, int y) {
+    static int have_warned = 0;
     int offset = y * WIDTH + x;
     int v = image_buffer[offset];
     if (v + PIXEL_INC >= 255) {
-        fprintf(stderr, "WARN: pixel value out of range (%d, %d)\n", x, y);
+        if (!have_warned) {
+            fprintf(stderr, "WARN: pixel value out of range (%d, %d)\n", x, y);
+            have_warned = 1;
+        }
     } else {
         v += PIXEL_INC;
         image_buffer[offset] = v;
@@ -31,118 +37,21 @@ void pixel_inc(uint8_t *image_buffer, int x, int y) {
 }
 
 void draw_line(uint8_t *image_buffer, int x1, int y1, int x2, int y2, int color) {
-    static int num_levels = 256;
-    static int intensity_bits = 8;
-    unsigned short intensity_shift, error_adj, error_acc;
-   unsigned short error_acc_temp, weighting, weighting_complement_mask;
-   short delta_x, delta_y, tmp, x_dir;
+  int dx = abs(x2 - x1);
+  int sx = x1 < x2 ? 1 : -1;
+  int dy = abs(y2-y1);
+  int sy = y1 < y2 ? 1 : -1;
+  int err = (dx > dy ? dx : -dy) / 2;
+  int e2;
 
-   /* Make sure the line runs top to bottom */
-   if (y1 > y2) {
-      tmp = y1; y1 = y2; y2 = tmp;
-      tmp = x1; x1 = x2; x2 = tmp;
-   }
-   /* Draw the initial pixel, which is always exactly intersected by
-      the line and so needs no weighting */
-   pixel_put(image_buffer, x1, y1, color);
-
-   if ((delta_x = x2 - x1) >= 0) {
-      x_dir = 1;
-   } else {
-      x_dir = -1;
-      delta_x = -delta_x; /* make delta_x positive */
-   }
-   /* Special-case horizontal, vertical, and diagonal lines, which
-      require no weighting because they go right through the center of
-      every pixel */
-   if ((delta_y = y2 - y1) == 0) {
-      /* Horizontal line */
-      while (delta_x-- != 0) {
-         x1 += x_dir;
-         pixel_put(image_buffer, x1, y1, color);
-      }
-      return;
-   }
-   if (delta_x == 0) {
-      /* Vertical line */
-      do {
-         y1++;
-         pixel_put(image_buffer,x1, y1, color);
-      } while (--delta_y != 0);
-      return;
-   }
-   if (delta_x == delta_y) {
-      /* Diagonal line */
-      do {
-         x1 += x_dir;
-         y1++;
-         pixel_put(image_buffer, x1, y1, color);
-      } while (--delta_y != 0);
-      return;
-   }
-   /* Line is not horizontal, diagonal, or vertical */
-   error_acc = 0;  /* initialize the line error accumulator to 0 */
-   /* # of bits by which to shift error_acc to get intensity level */
-   intensity_shift = 16 - intensity_bits;
-   /* Mask used to flip all bits in an intensity weighting, producing the
-      result (1 - intensity weighting) */
-   weighting_complement_mask = num_levels - 1;
-   /* Is this an X-major or Y-major line? */
-   if (delta_y > delta_x) {
-      /* Y-major line; calculate 16-bit fixed-point fractional part of a
-         pixel that X advances each time Y advances 1 pixel, truncating the
-         result so that we won't overrun the endpoint along the X axis */
-      error_adj = ((unsigned long) delta_x << 16) / (unsigned long) delta_y;
-      /* Draw all pixels other than the first and last */
-      while (--delta_y) {
-         error_acc_temp = error_acc;   /* remember currrent accumulated error */
-         error_acc += error_adj;      /* calculate error for next pixel */
-         if (error_acc <= error_acc_temp) {
-            /* The error accumulator turned over, so advance the X coord */
-            x1 += x_dir;
-         }
-         y1++; /* Y-major, so always advance Y */
-         /* The intensity_bits most significant bits of error_acc give us the
-            intensity weighting for this pixel, and the complement of the
-            weighting for the paired pixel */
-         weighting = error_acc >> intensity_shift;
-         pixel_put(image_buffer, x1, y1, color + weighting);
-         pixel_put(image_buffer, x1 + x_dir, y1,
-               color + (weighting ^ weighting_complement_mask));
-      }
-      /* Draw the final pixel, which is
-         always exactly intersected by the line
-         and so needs no weighting */
-      pixel_put(image_buffer, x2, y2, color);
-      return;
-   }
-   /* It's an X-major line; calculate 16-bit fixed-point fractional part of a
-      pixel that Y advances each time X advances 1 pixel, truncating the
-      result to avoid overrunning the endpoint along the X axis */
-   error_adj = ((unsigned long) delta_y << 16) / (unsigned long) delta_x;
-   /* Draw all pixels other than the first and last */
-   while (--delta_x) {
-      error_acc_temp = error_acc;   /* remember currrent accumulated error */
-      error_acc += error_adj;      /* calculate error for next pixel */
-      if (error_acc <= error_acc_temp) {
-         /* The error accumulator turned over, so advance the Y coord */
-         y1++;
-      }
-      x1 += x_dir; /* X-major, so always advance X */
-      /* The intensity_bits most significant bits of error_acc give us the
-         intensity weighting for this pixel, and the complement of the
-         weighting for the paired pixel */
-      weighting = error_acc >> intensity_shift;
-      pixel_put(image_buffer, x1, y1, color + weighting);
-      pixel_put(image_buffer, x1, y1 + 1,
-            color + (weighting ^ weighting_complement_mask));
-   }
-   /* Draw the final pixel, which is always exactly intersected by the line
-      and so needs no weighting */
-   pixel_put(image_buffer, x2, y2, color);
+  for(;;){
+    pixel_inc(image_buffer, x1, y1);
+    if (x1 == x2 && y1 == y2) break;
+    e2 = err;
+    if (e2 > -dx) { err -= dy; x1 += sx; }
+    if (e2 <  dy) { err += dx; y1 += sy; }
+  }
 }
-
-
 
 int main() {
     FILE *fp = fopen("../rfdata/rf-2.500-1.raw", "r");
@@ -162,12 +71,12 @@ int main() {
 
     int fname_index = 1;
     for (int j = 0; j < size; j += SAMPLES_STEP) {
-        memset(image_buffer, 255, WIDTH * HEIGHT * sizeof(uint8_t));
+        memset(image_buffer, 0, WIDTH * HEIGHT * sizeof(uint8_t));
         for (int i = 0; i < j; i += 2) {
             int x2 = (sample_buffer[i] + 128) % 256;
             int y2 = (sample_buffer[i + 1] + 128) % 256;
             if (i > 0) {
-                draw_line(image_buffer, x1, y1, x2, y2, 0);
+                draw_line(image_buffer, x1 * SIZE_MULTIPLIER, y1 * SIZE_MULTIPLIER, x2 * SIZE_MULTIPLIER, y2 * SIZE_MULTIPLIER, 0);
             }
             x1 = x2;
             y1 = y2;
