b3fc725bd7ed468c9cd9be3e83385ba2c7a59ee0
pieter heremans <pieter.heremans@gmail.com>
2015-02-03 15:07:35 +0000

    rpi iqvis working (low framerate though)

--- rpi-fdb/iqvis.c
+++ rpi-fdb/iqvis.c
@@ -1,14 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+// Visualisation on the Raspberry PI
+
 #include <math.h>
-#include <assert.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include <unistd.h>
+#include <assert.h>
+
+
+#include <rtl-sdr.h>
 
 #include "bcm_host.h"
 #include "GLES2/gl2.h"
 #include "EGL/egl.h"
 #include "EGL/eglext.h"
+
+
+#define WIDTH 256
+#define HEIGHT 256
 
 EGL_DISPMANX_WINDOW_T window;
 EGLDisplay display;
@@ -16,6 +26,356 @@ EGLSurface surface;
 EGLContext context;
 uint32_t screen_width;
 uint32_t screen_height;
+
+GLuint texture_id;
+GLuint program;
+GLuint position_vbo;
+GLuint uv_vbo;
+GLuint vao;
+uint8_t * buffer=0; 
+rtlsdr_dev_t *device;
+pthread_t receive_thread;
+double freq_mhz = 124.2;
+int paused = 0;
+int intensity = 3;
+void *rtl_buffer;
+const uint32_t rtl_buffer_length = (16 * 16384);
+int rtl_should_quit = 0;
+
+void rtl_check_status(rtlsdr_dev_t *device, int status, const char *message, const char *file, int line) {
+    if (status != 0) {
+        fprintf(stderr, "RTL-SDR: %s (Status code %d) %s:%d\n", message, status, file, line);
+        if (device != NULL) {
+            rtlsdr_close(device);
+        }
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define RTL_CHECK_STATUS(device, status, message) rtl_check_status(device, status, message, __FILE__, __LINE__)
+
+void receive_block(unsigned char *in_buffer, uint32_t buffer_length, void *ctx) {
+    if (paused) return;
+    memset(buffer, 0x0, WIDTH * HEIGHT * 3 );
+    int i = 0;
+    for (i = 0; i < buffer_length; i += 2) {
+        int vi = in_buffer[i];
+        int vq = in_buffer[i + 1];
+
+        int d = ((vq * WIDTH) + vi) * 3;
+        uint8_t v = buffer[d];
+        v += intensity;
+        v = v < 0 ? 0 : v > 255 ? 255 : v;
+        //if (i == 100) printf("i %d q %d d %d v %d\n", vi, vq, d, v);
+        buffer[d] = v;
+    }
+    //sleep(1);
+}
+
+// This function will block, so needs to be called on its own thread.
+void *_receive_loop(rtlsdr_dev_t *device) {
+    while (!rtl_should_quit) {
+        int n_read;
+        int status = rtlsdr_read_sync(device, rtl_buffer, rtl_buffer_length, &n_read);
+        RTL_CHECK_STATUS(device, status, "rtlsdr_read_sync");
+
+        if (n_read < rtl_buffer_length) {
+            fprintf(stderr, "Short read, samples lost, exiting!\n");
+            exit(EXIT_FAILURE);
+        }
+
+        receive_block(rtl_buffer, rtl_buffer_length, device);
+    }
+    return NULL;
+}
+
+static void setup_rtl() {
+    int status;
+
+    rtl_buffer = calloc(rtl_buffer_length, sizeof(uint8_t));
+
+    int device_count = rtlsdr_get_device_count();
+    if (device_count == 0) {
+        fprintf(stderr, "RTL-SDR: No devices found.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    const char *device_name = rtlsdr_get_device_name(0);
+    printf("Device %s\n", device_name);
+
+    status = rtlsdr_open(&device, 0);
+    RTL_CHECK_STATUS(device, status, "rtlsdr_open");
+
+    status = rtlsdr_set_sample_rate(device, 2e6);
+    RTL_CHECK_STATUS(device, status, "rtlsdr_set_sample_rate");
+
+    // Set auto-gain mode
+    status = rtlsdr_set_tuner_gain_mode(device, 0);
+    RTL_CHECK_STATUS(device, status, "rtlsdr_set_tuner_gain_mode");
+
+    status = rtlsdr_set_agc_mode(device, 1);
+    RTL_CHECK_STATUS(device, status, "rtlsdr_set_agc_mode");
+
+    status = rtlsdr_set_center_freq(device, freq_mhz * 1e6);
+    RTL_CHECK_STATUS(device, status, "rtlsdr_set_center_freq");
+
+    status = rtlsdr_reset_buffer(device);
+    RTL_CHECK_STATUS(device, status, "rtlsdr_reset_buffer");
+
+    printf("Start\n");
+    pthread_create(&receive_thread, NULL, (void *(*)(void *))_receive_loop, device);
+    printf("Running\n");
+
+}
+
+static void set_frequency() {
+    freq_mhz = round(freq_mhz * 10.0) / 10.0;
+    printf("Seting freq to %f MHz.\n", freq_mhz);
+    int status = rtlsdr_set_center_freq(device, freq_mhz * 1e6);
+    RTL_CHECK_STATUS(device, status, "rtlsdr_set_center_freq");
+}
+
+static void teardown_rtl() {
+    int status;
+
+    rtl_should_quit = 1;
+
+    printf("pthread_join\n");
+    pthread_join(receive_thread, NULL);
+
+    printf("rtlsdr_close\n");
+    status = rtlsdr_close(device);
+    //printf("Closed\n");
+    RTL_CHECK_STATUS(device, status, "rtlsdr_close");
+}
+
+
+void ngl_check_gl_error(const char *file, int line) {
+    GLenum err = glGetError();
+    int has_error = 0;
+    while (err != GL_NO_ERROR) {
+        has_error = 1;
+        char *msg = NULL;
+        switch(err) {
+            case GL_INVALID_OPERATION:
+            msg = "GL_INVALID_OPERATION";
+            break;
+            case GL_INVALID_ENUM:
+            msg = "GL_INVALID_ENUM";
+            fprintf(stderr, "OpenGL error: GL_INVALID_ENUM\n");
+            break;
+            case GL_INVALID_VALUE:
+            msg = "GL_INVALID_VALUE";
+            fprintf(stderr, "OpenGL error: GL_INVALID_VALUE\n");
+            break;
+            case GL_OUT_OF_MEMORY:
+            msg = "GL_OUT_OF_MEMORY";
+            fprintf(stderr, "OpenGL error: GL_OUT_OF_MEMORY\n");
+            break;
+            default:
+            msg = "UNKNOWN_ERROR";
+        }
+        fprintf(stderr, "OpenGL error: %s - %s:%d\n", msg, file, line);
+        err = glGetError();
+    }
+    if (has_error) {
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define NGL_CHECK_ERROR() ngl_check_gl_error(__FILE__, __LINE__)
+
+static void check_shader_error(GLuint shader) {
+    int length = 0;
+
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &length);
+
+    if (length > 0) {
+        char message[length];
+        glGetShaderInfoLog(shader, length, NULL, message);
+        printf("%s\n", message);
+    }
+}
+
+
+static const GLfloat positions[] = {
+    -1.0, -1.0,
+     1.0, -1.0,
+    -1.0,  1.0,
+     1.0,  1.0
+};
+
+static const GLfloat uvs[] = {
+    1.0, 1.0,
+    1.0, 0.0,
+    0.0, 1.0,
+    0.0, 0.0
+};
+
+GLuint u_texture;
+static void setup() {
+    glPixelStorei ( GL_UNPACK_ALIGNMENT, 1 );
+    NGL_CHECK_ERROR();
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    NGL_CHECK_ERROR();
+
+    glGenTextures(1, &texture_id);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,GL_CLAMP_TO_EDGE );
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T,GL_CLAMP_TO_EDGE );
+    NGL_CHECK_ERROR();
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, buffer);
+    NGL_CHECK_ERROR();
+    //glBindTexture(GL_TEXTURE_2D, 0);
+    //NGL_CHECK_ERROR();
+
+    const char *vertex_shader_source =
+        "#ifdef GL_ES\n"
+        "precision mediump float;\n"
+        "#endif\n"
+        "attribute vec2 vp;\n"
+        "attribute vec2 vt;\n"
+        "varying vec2 uv;\n"
+        "void main(void) {\n"
+        "  uv = vt;\n"
+        "  gl_Position = vec4(vp.x, vp.y, 0, 1);\n"
+        "}\n";
+
+    const char *fragment_shader_source =
+        "#ifdef GL_ES\n"
+        "precision mediump float;\n"
+        "#endif\n"
+        "uniform sampler2D texture;\n"
+        "varying vec2 uv;\n"
+        "void main(void) {\n"
+        "  vec4 c = texture2D(texture, uv);\n"
+        "  float v = c.r;\n"
+        "  gl_FragColor = vec4(v, v, v, 1);\n"
+        "}\n";
+
+    GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    check_shader_error(vertex_shader);
+
+    GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+    check_shader_error(fragment_shader);
+
+    program = glCreateProgram();
+    glAttachShader(program, vertex_shader);
+    glAttachShader(program, fragment_shader);
+    glLinkProgram(program);
+    NGL_CHECK_ERROR();
+
+    glUseProgram(program);
+    NGL_CHECK_ERROR();
+
+    u_texture = glGetUniformLocation(program, "texture");
+    glUniform1i(u_texture, 0);
+
+    NGL_CHECK_ERROR();
+
+    GLuint a_vp, a_vt;
+    a_vp = glGetAttribLocation(program, "vp");
+    a_vt = glGetAttribLocation(program, "vt");
+    glVertexAttribPointer(a_vp, 2, GL_FLOAT, 0, 0, positions);
+    glEnableVertexAttribArray(a_vp);
+    glVertexAttribPointer(a_vt, 2, GL_FLOAT, 0, 0, uvs);
+    glEnableVertexAttribArray(a_vt);
+    NGL_CHECK_ERROR();
+
+
+
+}
+
+static void prepare() {
+    uint32_t width, height;
+
+    int32_t success = graphics_get_display_size(0, &width, &height);
+    assert(success >= 0);
+
+    glViewport(0, 0, width, height);
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    NGL_CHECK_ERROR();
+}
+
+static void update() {
+    
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    NGL_CHECK_ERROR();
+    //printf("Up: %d\n", buffer[(128*WIDTH + 128) * 3]);
+    //FILE *fp = fopen("buf.raw", "wb");
+    //fwrite(buffer, 1, WIDTH * HEIGHT * 3, fp);
+    //fclose(fp);
+    //sleep(1);
+    //glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, WIDTH, HEIGHT, GL_RGB, GL_UNSIGNED_BYTE, buffer);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, WIDTH, HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, buffer);
+    NGL_CHECK_ERROR();
+}
+
+static void draw() {
+    //glEnable(GL_TEXTURE_2D);
+    //NGL_CHECK_ERROR();
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_ONE, GL_SRC_COLOR);
+    //NGL_CHECK_ERROR();
+
+    glUseProgram(program);
+    NGL_CHECK_ERROR();
+    //glActiveTexture ( GL_TEXTURE0 );
+    //glBindTexture ( GL_TEXTURE_2D, texture_id );
+    NGL_CHECK_ERROR();
+    glUniform1i(u_texture,0);
+    NGL_CHECK_ERROR();
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
+    NGL_CHECK_ERROR();
+
+    //glUseProgram(0);
+    //NGL_CHECK_ERROR();
+
+    glFlush();
+    //glFinish();
+    //NGL_CHECK_ERROR();
+}
+
+/*
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    } else if (key == GLFW_KEY_RIGHT && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
+        if (mods == 0) {
+            freq_mhz += 0.1;
+        } else {
+            freq_mhz += 10;
+        }
+        set_frequency();
+    } else if (key == GLFW_KEY_LEFT && (action == GLFW_PRESS || action == GLFW_REPEAT)) {
+        if (mods == 0) {
+            freq_mhz -= 0.1;
+        } else {
+            freq_mhz -= 10;
+        }
+        set_frequency();
+    } else if (key == GLFW_KEY_SPACE && action == GLFW_PRESS) {
+        paused = !paused;
+    } else if (key == GLFW_KEY_EQUAL && action == GLFW_PRESS) {
+        intensity += 0.01;
+        printf("Intensity: %.2f\n", intensity);
+    } else if (key == GLFW_KEY_MINUS && action == GLFW_PRESS) {
+        intensity -= 0.01;
+        printf("Intensity: %.2f\n", intensity);
+    }
+}
+*/
 
 void nwm_init() {
     EGLBoolean result;
@@ -89,14 +449,27 @@ void nwm_init() {
     result = eglMakeCurrent(display, surface, surface, context);
     assert(result != EGL_FALSE);
 
-    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
 }
 
-int main() {
+
+int main(void) {
+    buffer =  malloc(WIDTH * HEIGHT * 3);
     bcm_host_init();
     nwm_init();
-    eglSwapBuffers(display, surface);
-    sleep(2);
-    return 0;
+
+    setup_rtl();
+    setup();
+
+   freq_mhz=202.5;
+   set_frequency();
+
+    while ( 1) {
+       prepare();
+      update();
+       draw();
+       eglSwapBuffers(display, surface);
+       //sleep(0);
+    }
+      teardown_rtl();
+      exit(EXIT_SUCCESS);
 }
