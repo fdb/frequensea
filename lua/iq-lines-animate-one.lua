8c666f3209e3bb0f858f8d6b4879dd6c8ec3f061
Frederik De Bleser <frederik@debleser.be>
2015-02-21 22:43:18 +0100

    Fix iq-lines-animate-one.

--- lua/iq-lines-animate-one.lua
+++ lua/iq-lines-animate-one.lua
@@ -1,46 +1,61 @@
--- Visualize IQ data from the HackRF
--- This loads one second of data and then visualizes it.
--- Currently segfaults.
+-- Visualize a single sample
 
 VERTEX_SHADER = [[
 #version 400
 layout (location = 0) in vec3 vp;
 layout (location = 1) in vec3 vn;
+layout (location = 2) in vec2 vt;
 out vec3 color;
+out vec2 texCoord;
 uniform mat4 uViewMatrix, uProjectionMatrix;
 uniform float uTime;
 void main() {
     color = vec3(1.0, 1.0, 1.0);
-    vec2 pt = vec2((vp.x - 0.5) * 1.8, (vp.y - 0.5) * 1.8);
-    gl_Position = vec4(pt.x, pt.y, 0.0, 1.0);
+    texCoord = vt;
+    gl_Position = vec4(vp.x*2, vp.z*2, 0, 1.0);
 }
 ]]
 
 FRAGMENT_SHADER = [[
 #version 400
 in vec3 color;
+in vec2 texCoord;
+uniform sampler2D uTexture;
 layout (location = 0) out vec4 fragColor;
 void main() {
-    fragColor = vec4(color, 0.95);
+    float r = texture(uTexture, texCoord).r * 100;
+    fragColor = vec4(r, r, r, 1);
 }
 ]]
 
 function setup()
-    -- Start the device for a minute, wait a bit, and stop again
-    device = nrf_device_new(2.5, "../rfdata/rf-2.500-1.raw")
-    os.execute("sleep " .. 1)
-    nrf_device_free(device)
+    countdown = 10
+    percentage = 0
+    device = nrf_device_new(100.9, "../rfdata/rf-2.500-1.raw")
+    filter = nrf_iq_filter_new(device.sample_rate, 200e3, 51)
 
     camera = ngl_camera_new_look_at(0, 0, 0) -- Shader ignores camera position, but camera object is required for ngl_draw_model
-    shader = ngl_shader_new(GL_LINE_STRIP, VERTEX_SHADER, FRAGMENT_SHADER)
-
-    samples = 0
+    shader = ngl_shader_new(GL_TRIANGLES, VERTEX_SHADER, FRAGMENT_SHADER)
+    texture = ngl_texture_new(shader, "uTexture")
+    model = ngl_model_new_grid_triangles(2, 2, 1, 1)
 end
 
 function draw()
-    ngl_clear(0.2, 0.2, 0.2, 1.0)
-    buffer = nrf_device_get_samples_buffer(device)
-    model = ngl_model_new(buffer.channels, math.min(samples, buffer.width * buffer.height), buffer.data)
-    ngl_draw_model(camera, model, shader)
-    samples = samples + 10
+    if countdown > 0 then
+        samples_buffer = nrf_device_get_samples_buffer(device)
+        nrf_iq_filter_process(filter, samples_buffer)
+        filter_buffer = nrf_iq_filter_get_buffer(filter)
+        countdown = countdown - 1
+    elseif percentage > 1 then
+        countdown = 1
+        percentage = 0
+    else
+        iq_buffer = nrf_buffer_to_iq_lines(filter_buffer, 4, percentage)
+
+        ngl_clear(0.2, 0.2, 0.2, 1.0)
+        ngl_texture_update(texture, iq_buffer, 1024, 1024)
+        ngl_draw_model(camera, model, shader)
+
+        percentage = percentage + 0.0001
+    end
 end
