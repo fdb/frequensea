4fd65e5bbad307e9b94984648c25868a810e9bdc
Frederik De Bleser <frederik@debleser.be>
2015-02-22 15:54:46 +0100

    Broad spectrum gettin'

---
+++ c/fft-batch-broad.c
@@ -0,0 +1,193 @@
+// Batch export of FFT data as images.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+
+#include <libhackrf/hackrf.h>
+#include <fftw3.h>
+
+#include "easypng.h"
+
+const uint32_t FFT_SIZE = 4096;
+const uint32_t FFT_HISTORY_SIZE = 4096;
+const uint32_t SAMPLES_SIZE = 131072;
+const uint64_t FREQUENCY_START = 25.00001e6;
+const uint64_t FREQUENCY_END = 6000e6;
+const uint32_t FREQUENCY_STEP = 5e6;
+const uint32_t SAMPLE_RATE = 5e6;
+const uint32_t SAMPLE_BLOCKS_TO_SKIP = 10;
+
+uint64_t frequency = FREQUENCY_START;
+
+fftw_complex *fft_in;
+fftw_complex *fft_out;
+fftw_complex *fft_history;
+int history_rows = 0;
+fftw_plan fft_plan;
+hackrf_device *device;
+int skip = SAMPLE_BLOCKS_TO_SKIP;
+time_t start_time, end_time;
+
+// Utility ////////////////////////////////////////////////////////////////////
+
+uint8_t clamp_u8(int v, uint8_t min, uint8_t max) {
+    return (uint8_t) (v < min ? min : v > max ? max : v);
+}
+
+// HackRF /////////////////////////////////////////////////////////////////////
+
+static void hackrf_check_status(int status, const char *message, const char *file, int line) {
+    if (status != 0) {
+        fprintf(stderr, "NRF HackRF fatal error: %s\n", message);
+        if (device != NULL) {
+            hackrf_close(device);
+        }
+        hackrf_exit();
+        exit(EXIT_FAILURE);
+    }
+}
+
+#define HACKRF_CHECK_STATUS(status, message) hackrf_check_status(status, message, __FILE__, __LINE__)
+
+int receive_sample_block(hackrf_transfer *transfer) {
+    uint64_t local_frequency = frequency;
+    if (skip > 0) {
+        skip--;
+        return 0;
+    }
+    if (history_rows >= FFT_HISTORY_SIZE) return 0;
+    int ii = 0;
+    for (int i = 0; i < SAMPLES_SIZE; i += 2) {
+        int vi = (transfer->buffer[i] + 128) % 256;
+        int vq = (transfer->buffer[i + 1] + 128) % 256;
+        fft_in[ii][0] = powf(-1, ii) * vi / 256.0;
+        fft_in[ii][1] = powf(-1, ii) * vq / 256.0;
+        ii++;
+    }
+    fftw_execute(fft_plan);
+
+    // Move one line down.
+    memcpy(fft_history + FFT_SIZE, fft_history, FFT_SIZE * (FFT_HISTORY_SIZE - 1) * sizeof(fftw_complex));
+    // Set the first line.
+    memcpy(fft_history, fft_out, FFT_SIZE * sizeof(fftw_complex));
+
+    history_rows++;
+    printf("\r%.f%%", history_rows / (float)FFT_HISTORY_SIZE * 100);
+    fflush(stdout);
+    if (history_rows >= FFT_HISTORY_SIZE) {
+        printf("\n");
+        // Write image.
+        uint8_t *buffer = calloc(FFT_SIZE * FFT_HISTORY_SIZE, sizeof(uint8_t));
+        for (int y = 0; y < FFT_HISTORY_SIZE; y++) {
+            for (int x = 0; x < FFT_SIZE; x++) {
+                double ci = fft_history[y * FFT_SIZE + x][0];
+                double cq = fft_history[y * FFT_SIZE + x][1];
+                double pwr = ci * ci + cq * cq;
+                //double pwr_dbfs = 10.0 * log2(pwr + 1.0e-20) / log2(2.7182818284);
+                double pwr_dbfs = 10.0 * log10(pwr + 1.0e-20);
+                pwr_dbfs = pwr_dbfs * 5;
+                uint8_t v = clamp_u8(pwr_dbfs, 0, 255);
+                if (x == FFT_SIZE / 2) {
+                    v = buffer[y * FFT_SIZE + x - 1];
+                }
+                buffer[y * FFT_SIZE + x] = v;
+            }
+        }
+        char file_name[100];
+        snprintf(file_name, 100, "broad-%.0f.png", local_frequency / 1.0e6);
+        write_gray_png(file_name, FFT_SIZE, FFT_HISTORY_SIZE, buffer);
+
+        free(buffer);
+    }
+
+    return 0;
+}
+
+static void setup_hackrf() {
+    int status;
+
+    status = hackrf_init();
+    HACKRF_CHECK_STATUS(status, "hackrf_init");
+
+    status = hackrf_open(&device);
+    HACKRF_CHECK_STATUS(status, "hackrf_open");
+
+    status = hackrf_set_freq(device, frequency);
+    HACKRF_CHECK_STATUS(status, "hackrf_set_freq");
+
+    status = hackrf_set_sample_rate(device, SAMPLE_RATE);
+    HACKRF_CHECK_STATUS(status, "hackrf_set_sample_rate");
+
+    status = hackrf_set_amp_enable(device, 0);
+    HACKRF_CHECK_STATUS(status, "hackrf_set_amp_enable");
+
+    status = hackrf_set_lna_gain(device, 32);
+    HACKRF_CHECK_STATUS(status, "hackrf_set_lna_gain");
+
+    status = hackrf_set_vga_gain(device, 36);
+    HACKRF_CHECK_STATUS(status, "hackrf_set_lna_gain");
+
+    status = hackrf_start_rx(device, receive_sample_block, NULL);
+    HACKRF_CHECK_STATUS(status, "hackrf_start_rx");
+}
+
+static void teardown_hackrf() {
+    hackrf_stop_rx(device);
+    hackrf_close(device);
+    hackrf_exit();
+}
+
+// FFTW /////////////////////////////////////////////////////////////////////
+
+static void setup_fftw() {
+    fft_in = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * SAMPLES_SIZE);
+    fft_out = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * SAMPLES_SIZE);
+    fft_history = (fftw_complex*) fftw_malloc(sizeof(fftw_complex) * FFT_SIZE * FFT_HISTORY_SIZE);
+    fft_plan = fftw_plan_dft_1d(FFT_SIZE, fft_in, fft_out, FFTW_FORWARD, FFTW_ESTIMATE);
+}
+
+static void teardown_fftw() {
+    fftw_destroy_plan(fft_plan);
+    fftw_free(fft_in);
+    fftw_free(fft_out);
+    fftw_free(fft_history);
+}
+
+// Main /////////////////////////////////////////////////////////////////////
+
+int main(int argc, char **argv) {
+    setup_fftw();
+    setup_hackrf();
+    printf("Frequency: %.4f MHz\n", frequency / 1.0e6);
+    time(&start_time);
+
+    while (frequency <= FREQUENCY_END) {
+        while (history_rows < FFT_HISTORY_SIZE) {
+            sleep(1);
+        }
+
+        time(&end_time);
+        printf("Elapsed: %.0f seconds.\n", difftime(end_time, start_time));
+
+        frequency = frequency + FREQUENCY_STEP;
+        if (frequency > FREQUENCY_END) {
+            exit(0);
+        }
+
+        int status = hackrf_set_freq(device, frequency);
+        HACKRF_CHECK_STATUS(status, "hackrf_set_freq");
+
+        skip = SAMPLE_BLOCKS_TO_SKIP;
+        history_rows = 0;
+        printf("Frequency: %.4f MHz\n", frequency / 1.0e6);
+        time(&start_time);
+    }
+
+    teardown_hackrf();
+    teardown_fftw();
+
+    return 0;
+}
---
+++ c/fft-stitch-broad.c
@@ -0,0 +1,227 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <math.h>
+#include <string.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../externals/stb/stb_image.h"
+#define STB_TRUETYPE_IMPLEMENTATION
+#include "../externals/stb/stb_truetype.h"
+
+#include "easypng.h"
+
+// Stitch FFT sweeps PNG
+
+const uint32_t IMAGE_HEIGHT = 900;
+const uint32_t FOOTER_HEIGHT = 600;
+const uint32_t FFT_SIZE = 4096;
+const uint32_t FFT_HISTORY_SIZE = IMAGE_HEIGHT - FOOTER_HEIGHT;
+const uint64_t FREQUENCY_START = 200e6;
+const uint64_t FREQUENCY_END = 250e6;
+const uint32_t FREQUENCY_STEP = 5e6;
+
+const uint32_t SAMPLE_RATE = 10e6;
+const uint32_t WIDTH_STEP = FFT_SIZE / (SAMPLE_RATE / FREQUENCY_STEP);
+const uint32_t FREQUENCY_RANGE = (FREQUENCY_END - FREQUENCY_START) / FREQUENCY_STEP;
+const uint32_t IMAGE_WIDTH = FFT_SIZE + (FREQUENCY_RANGE) * WIDTH_STEP;
+const uint32_t MINOR_TICK_RATE = 0.1e6;
+const double MINOR_TICK_SIZE = (FFT_SIZE / (double) FREQUENCY_STEP / 2) * MINOR_TICK_RATE;
+const uint32_t MAJOR_TICK_RATE = 1e6;
+const double MAJOR_TICK_SIZE = (FFT_SIZE / (double) FREQUENCY_STEP / 2) * MAJOR_TICK_RATE;
+const uint8_t LINE_COLOR = 255;
+const char* FONT_FILE = "../fonts/RobotoCondensed-Regular.ttf";
+const uint16_t FONT_SIZE_PX = 48;
+const uint32_t MARKERS_Y = FFT_HISTORY_SIZE + (FOOTER_HEIGHT / 2 - FONT_SIZE_PX / 2);
+const uint32_t MARKERS_X = FFT_SIZE-SAMPLE_RATE / 2;
+
+// Utility //////////////////////////////////////////////////////////////////
+
+uint8_t max_u8(uint8_t a, uint8_t b) {
+    return a > b ? a : b;
+}
+
+// Image operations /////////////////////////////////////////////////////////
+
+void img_gray_copy(uint8_t *dst, uint8_t *src, uint32_t dst_x, uint32_t dst_y, uint32_t src_x, uint32_t src_y, uint32_t width, uint32_t height, uint32_t dst_stride, uint32_t src_stride) {
+    for (uint32_t i = 0; i < height; i++) {
+        for (uint32_t j = 0; j < width; j++) {
+            uint32_t dst_offset = (dst_y + i) * dst_stride + dst_x + j;
+            uint32_t src_offset = (src_y + i) * src_stride + src_x + j;
+            dst[dst_offset] = max_u8(dst[dst_offset], src[src_offset]);
+        }
+    }
+}
+
+void img_pixel_put(uint8_t *buffer, uint32_t stride, uint32_t x, uint32_t y, uint8_t v) {
+    if (x > 0 && y > 0) {
+        buffer[(y * stride) + x] = v;
+    }
+}
+
+void img_vline(uint8_t *buffer, uint32_t stride, uint32_t x1, uint32_t y1, uint32_t y2, uint8_t v) {
+    for (int y = y1; y < y2; y++) {
+        img_pixel_put(buffer, stride, x1, y, v);
+    }
+}
+
+void img_hline(uint8_t *buffer, uint32_t stride, uint32_t x1, uint32_t y1, uint32_t x2, uint8_t v) {
+    for (int x = x1; x < x2; x++) {
+        img_pixel_put(buffer, stride, x, y1, v);
+    }
+}
+
+// Font operations //////////////////////////////////////////////////////////
+
+typedef struct {
+    stbtt_fontinfo font;
+    uint8_t *buffer;
+} ntt_font;
+
+ntt_font *ntt_font_load(const char *font_file) {
+    ntt_font *font = calloc(1, sizeof(ntt_font));
+
+    FILE *fp = fopen(font_file, "rb");
+    fseek(fp, 0L, SEEK_END);
+    long size = ftell(fp);
+    rewind(fp);
+    font->buffer = calloc(size, 1);
+    fread(font->buffer, size, 1, fp);
+    fclose(fp);
+
+    stbtt_InitFont(&font->font, font->buffer, stbtt_GetFontOffsetForIndex(font->buffer, 0));
+    return font;
+}
+
+
+void ntt_font_measure(const ntt_font *font, const char *text, const int x, const int y, const int font_size, int *width, int *height) {
+    float font_scale = stbtt_ScaleForPixelHeight(&font->font, font_size);
+    int ascent, descent;
+    stbtt_GetFontVMetrics(&font->font, &ascent, &descent, 0);
+    int baseline = (int) (ascent * font_scale);
+    int descent_scaled = (int) (descent * font_scale);
+    //printf("Baseline %d descent %d\n", baseline, descent_scaled);
+
+    *width = x;
+    // The glyph height is constant for the font.
+    // Descent_scaled is negative.
+    *height = y + (baseline - descent_scaled);
+
+    int ch = 0;
+    while (text[ch]) {
+        int x0, y0, x1, y1;
+        int advance, lsb;
+        char c = text[ch];
+        stbtt_GetCodepointBitmapBox(&font->font, c, font_scale, font_scale, &x0, &y0, &x1, &y1);
+        stbtt_GetCodepointHMetrics(&font->font, c, &advance, &lsb);
+        int glyph_width = advance * font_scale;
+        *width += glyph_width;
+        if (text[ch + 1]) {
+            *width += font_scale * stbtt_GetCodepointKernAdvance(&font->font, c, text[ch + 1]);
+        }
+        ch++;
+    }
+}
+
+void ntt_font_draw(const ntt_font *font, uint8_t *img, const uint32_t img_stride, const char *text, const int x, const int y, const int font_size) {
+    int text_width, text_height;
+    ntt_font_measure(font, text, 0, 0, font_size, &text_width, &text_height);
+    int start_x = x - text_width / 2;
+    if (start_x < 0) return;
+
+    float font_scale = stbtt_ScaleForPixelHeight(&font->font, font_size);
+    int ascent;
+    stbtt_GetFontVMetrics(&font->font, &ascent, 0, 0);
+    int baseline = (int) (ascent * font_scale);
+
+    int ch = 0;
+    int _x = 0;
+    while (text[ch]) {
+        int w, h, dx, dy;
+        int advance, lsb;
+        char c = text[ch];
+        stbtt_GetCodepointHMetrics(&font->font, c, &advance, &lsb);
+        uint8_t *glyph_bitmap = stbtt_GetCodepointBitmap(&font->font, font_scale, font_scale, c, &w, &h, &dx, &dy);
+        //printf("Offset %d %d\n", dx, baseline + dy);
+        img_gray_copy(img, glyph_bitmap, start_x + _x + dx, y + baseline + dy, 0, 0, w, h, img_stride, w);
+        //x += w;
+        _x += (advance * font_scale);
+
+        if (text[ch + 1]) {
+            _x += font_scale * stbtt_GetCodepointKernAdvance(&font->font, c, text[ch + 1]);
+        }
+
+        ch++;
+    }
+}
+
+// Main /////////////////////////////////////////////////////////////////////
+
+int main() {
+    ntt_font *font = ntt_font_load(FONT_FILE);
+    uint32_t image_height = IMAGE_HEIGHT;
+    printf("Image size: %d x %d\n", IMAGE_WIDTH, image_height);
+    uint8_t *buffer = calloc(IMAGE_WIDTH * image_height, sizeof(uint8_t));
+    //memset(buffer, 128, IMAGE_WIDTH * image_height);
+    uint32_t x = 0;
+    for (uint32_t frequency = FREQUENCY_START; frequency <= FREQUENCY_END; frequency += FREQUENCY_STEP) {
+        char file_name[100];
+        snprintf(file_name, 100, "broad-%.0f.png", frequency / 1.0e6);
+        printf("Composing %s...\n", file_name);
+
+        int width, height, n;
+        unsigned char *image_data = stbi_load(file_name, &width, &height, &n, 1);
+        if (!image_data) {
+            fprintf (stderr, "ERROR: could not load %s\n", file_name);
+            exit(1);
+        }
+        if (width != FFT_SIZE || height < FFT_HISTORY_SIZE) {
+            fprintf (stderr, "ERROR: bad image size %s\n", file_name);
+            exit(1);
+        }
+
+        // Compose image into buffer
+        img_gray_copy(buffer, image_data, x, 0, 0, 0, FFT_SIZE, FFT_HISTORY_SIZE, IMAGE_WIDTH, FFT_SIZE);
+
+        stbi_image_free(image_data);
+
+        x += WIDTH_STEP;
+    }
+
+    printf("Adding markers...\n");
+
+    int banner_y = FFT_HISTORY_SIZE;
+    int banner_bottom = image_height;
+    for (int i = 0; i < 10; i++) {
+        img_hline(buffer, IMAGE_WIDTH, 0, banner_y++, IMAGE_WIDTH, LINE_COLOR);
+        img_hline(buffer, IMAGE_WIDTH, 0, banner_bottom--, IMAGE_WIDTH, LINE_COLOR);
+    }
+    banner_bottom++;
+
+    for (double x = 0; x < IMAGE_WIDTH; x += MINOR_TICK_SIZE) {
+        img_vline(buffer, IMAGE_WIDTH, x, banner_y, banner_y + 50, LINE_COLOR);
+        img_vline(buffer, IMAGE_WIDTH, x, banner_bottom - 50, banner_bottom, LINE_COLOR);
+    }
+
+    int freq = FREQUENCY_START - (SAMPLE_RATE / 2) + (MAJOR_TICK_RATE / 2);
+    double start_x = FFT_SIZE / (double) SAMPLE_RATE * (MAJOR_TICK_RATE / 2);
+    for (double x = start_x; x < IMAGE_WIDTH; x += MAJOR_TICK_SIZE) {
+        img_vline(buffer, IMAGE_WIDTH, x, banner_y, banner_y + 100, LINE_COLOR);
+        img_vline(buffer, IMAGE_WIDTH, x, banner_bottom - 100, banner_bottom, LINE_COLOR);
+        if (freq >= 0 && freq < FREQUENCY_END + (SAMPLE_RATE / 2)) {
+            char text[200];
+            snprintf(text, 200, "%.2f", (freq / (double) 1e6));
+            ntt_font_draw(font, buffer, IMAGE_WIDTH, text, x, MARKERS_Y, FONT_SIZE_PX);
+        }
+        freq += MAJOR_TICK_RATE;
+    }
+
+
+    char out_file_name[100];
+    snprintf(out_file_name, 100, "fft-stitched-broad-%.0f-%.0f.png", FREQUENCY_START / 1e6, FREQUENCY_END / 1e6);
+    printf("Saving %s...\n", out_file_name);
+
+    write_gray_png(out_file_name, IMAGE_WIDTH, image_height, buffer);
+    exit(0);
+}
+
