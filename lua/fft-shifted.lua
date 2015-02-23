d80b416806e9859e295219621b4d5a09f42e3492
Frederik De Bleser <frederik@debleser.be>
2015-02-23 22:56:06 +0100

    Add a shifted FFT example.

---
+++ lua/fft-shifted.lua
@@ -0,0 +1,73 @@
+-- Visualize FFT data as a texture
+
+VERTEX_SHADER = [[
+#version 400
+layout (location = 0) in vec3 vp;
+layout (location = 1) in vec3 vn;
+layout (location = 2) in vec2 vt;
+out vec3 color;
+out vec2 texCoord;
+uniform mat4 uViewMatrix, uProjectionMatrix;
+uniform float uTime;
+void main() {
+    color = vec3(1.0, 1.0, 1.0);
+    texCoord = vt;
+    gl_Position = vec4(vp.x*2, vp.z*2, 0, 1.0);
+}
+]]
+
+FRAGMENT_SHADER = [[
+#version 400
+in vec3 color;
+in vec2 texCoord;
+uniform sampler2D uTexture;
+layout (location = 0) out vec4 fragColor;
+void main() {
+    float r = texture(uTexture, texCoord).r;
+    float g = texture(uTexture, texCoord).g;
+    float pwr = r * r + g * g;
+    float pwr_dbfs = 10.0 * log2(pwr + 1.0e-20) / log2(2.7182818284);
+
+    //float v = sqrt(r * r + g * g) * 0.1;
+    float v = pwr_dbfs * 0.02;
+    fragColor = vec4(v, v, v, 0.95);
+}
+]]
+
+function setup()
+    freq = 97.5
+    shift = 0
+    device = nrf_device_new(freq, "../rfdata/rf-200.500-big.raw")
+    shifter = nrf_freq_shifter_new(shift, device.sample_rate)
+    fft = nrf_fft_new(1024, 1024)
+
+    camera = ngl_camera_new_look_at(0, 0, 0) -- Camera is unnecessary but ngl_draw_model requires it
+    shader = ngl_shader_new(GL_TRIANGLES, VERTEX_SHADER, FRAGMENT_SHADER)
+    texture = ngl_texture_new(shader, "uTexture")
+    model = ngl_model_new_grid_triangles(2, 2, 1, 1)
+end
+
+function draw()
+    samples_buffer = nrf_device_get_samples_buffer(device)
+    nrf_freq_shifter_process(shifter, samples_buffer)
+    shifter_buffer = nrf_freq_shifter_get_buffer(shifter)
+    nrf_fft_process(fft, shifter_buffer)
+    fft_buffer = nrf_fft_get_buffer(fft)
+
+    ngl_clear(0.2, 0.2, 0.2, 1.0)
+    ngl_texture_update(texture, fft_buffer, 1024, 1024)
+    ngl_draw_model(camera, model, shader)
+end
+
+function on_key(key, mods)
+    keys_frequency_handler(key, mods)
+    if key == KEY_LEFT_BRACKET then
+        shift = shift - 10e3
+        shifter = nrf_freq_shifter_new(shift, device.sample_rate)
+        print("Shift: " .. shift)
+    elseif key == KEY_RIGHT_BRACKET then
+        shift = shift + 10e3
+        shifter = nrf_freq_shifter_new(shift, device.sample_rate)
+        print("Shift: " .. shift)
+    end
+end
