d87e58d4a5d8100dc63ed50e64c74707ae3e6858
Frederik De Bleser <frederik@debleser.be>
2015-02-24 15:27:26 +0100

    Save the buffer using the "E" key.

--- lua/iq-lines-buffer.lua
+++ lua/iq-lines-buffer.lua
@@ -52,4 +52,7 @@ end
 
 function on_key(key, mods)
     keys_frequency_handler(key, mods)
+    if key == KEY_E then
+        nul_buffer_save(nul_buffer_convert(filter_buffer, 1), "out.raw")
+    end
 end
