03b69f8dd0f18d28ceb832efcd231a0419ca92ea
Frederik De Bleser <frederik@debleser.be>
2015-02-27 21:48:23 +0100

    Use unfiltered buffer.

--- lua/iq-lines-buffer.lua
+++ lua/iq-lines-buffer.lua
@@ -23,15 +23,15 @@ in vec2 texCoord;
 uniform sampler2D uTexture;
 layout (location = 0) out vec4 fragColor;
 void main() {
-    float r = texture(uTexture, texCoord).r * 20;
+    float r = texture(uTexture, texCoord).r * 1;
     fragColor = vec4(r, r, r, 1);
 }
 ]]
 
 function setup()
-    freq = 604.1
+    freq = 433
     device = nrf_device_new(freq, "../rfdata/rf-200.500-big.raw")
-    filter = nrf_iq_filter_new(device.sample_rate, 100e3, 43)
+    --filter = nrf_iq_filter_new(device.sample_rate, 100e3, 43)
 
     camera = ngl_camera_new_look_at(0, 0, 0) -- Camera is unnecessary but ngl_draw_model requires it
     shader = ngl_shader_new(GL_TRIANGLES, VERTEX_SHADER, FRAGMENT_SHADER)
@@ -41,12 +41,12 @@ end
 
 function draw()
     samples_buffer = nrf_device_get_samples_buffer(device)
-    nrf_iq_filter_process(filter, samples_buffer)
-    filter_buffer = nrf_iq_filter_get_buffer(filter)
-    iq_buffer = nrf_buffer_to_iq_lines(filter_buffer, 4, 0.8)
+    --nrf_iq_filter_process(filter, samples_buffer)
+    --filter_buffer = nrf_iq_filter_get_buffer(filter)
+    iq_buffer = nrf_buffer_to_iq_lines(samples_buffer, 2, 1.0)
 
     ngl_clear(0.2, 0.2, 0.2, 1.0)
-    ngl_texture_update(texture, iq_buffer, 1024, 1024)
+    ngl_texture_update(texture, iq_buffer, 512, 512)
     ngl_draw_model(camera, model, shader)
 end
 
