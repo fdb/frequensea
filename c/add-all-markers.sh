4ef68e5c54ff4078965fa6a646e2da5d702dd2a5
Frederik De Bleser <frederik@debleser.be>
2015-02-27 14:17:07 +0100

    Script to add all markers to the file.

---
+++ c/add-all-markers.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+make add-markers
+./add-markers 10 465
+./add-markers 470 925
+./add-markers 930 1385
+./add-markers 1390 1845
+./add-markers 1850 2305
+./add-markers 2310 2765
+./add-markers 2770 3225
+./add-markers 3230 3685
+./add-markers 3690 4145
+./add-markers 4150 4605
