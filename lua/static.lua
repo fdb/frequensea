a462162d964041e682ca658f55fdb7702335e615
Frederik De Bleser <frederik@debleser.be>
2015-03-05 10:17:43 +0100

    Keys change the model.

--- lua/static.lua
+++ lua/static.lua
@@ -35,10 +35,27 @@ end
 
 function draw()
     ngl_clear(0.2, 0.2, 0.2, 1)
-    camera = ngl_camera_new_look_at(camera_x, camera_y, camera_z)
     ngl_draw_model(camera, model, shader)
 end
 
 function on_key(key, mods)
-    keys_camera_handler(key, mods)
+    if (mods == 1) then -- Shift key
+        d = 1
+    else
+        d = 0.1
+    end
+
+    if key == KEY_A or key == KEY_LEFT then
+        ngl_model_translate(model, d, 0.0, 0.0)
+    elseif key == KEY_D or key == KEY_RIGHT then
+        ngl_model_translate(model, -d, 0.0, 0.0)
+    elseif key == KEY_Q then
+        ngl_model_translate(model, 0.0, d, 0.0)
+    elseif key == KEY_E then
+        ngl_model_translate(model, 0.0, -d, 0.0)
+    elseif key == KEY_W or key == KEY_DOWN then
+        ngl_model_translate(model, 0.0, 0.0, d)
+    elseif key == KEY_S or key == KEY_DOWN then
+        ngl_model_translate(model, 0.0, 0.0, -d)
+    end
 end
