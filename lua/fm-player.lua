718ebf19ecfa61b1579ca760f426bb1e1c9e1a6f
Frederik De Bleser <frederik@debleser.be>
2019-08-27 20:02:59 +0200

    Show frequency in FM player.

--- lua/fm-player.lua
+++ lua/fm-player.lua
@@ -5,10 +5,12 @@ function setup()
     freq = (100.9e6 + freq_offset) / 1e6
     device = nrf_device_new(freq, "../rfdata/rf-100.900-1.raw")
     player = nrf_player_new(device, NRF_DEMODULATE_WBFM, freq_offset)
+    freq_font = ngl_font_new("../fonts/Roboto-Bold.ttf", 72)
 end
 
 function draw()
     ngl_clear(0.2, 0.2, 0.2, 1.0)
+    ngl_font_draw(freq_font, freq, 1, 0.7, 1.0)
 end
 
 function on_key(key, mods)
